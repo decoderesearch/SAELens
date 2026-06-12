899c564d93f3afba5e90ed5696f393a47dfcab65
Dan Raviv <dan@soundradix.com>
2026-06-12 13:01:40 -0700

    fix: Add missing hook_sae_acts_pre/post in JumpReLUTrainingSAE and TemporalSAE

--- sae_lens/saes/temporal_sae.py
+++ sae_lens/saes/temporal_sae.py
@@ -300,7 +300,8 @@ class TemporalSAE(SAE[TemporalSAEConfig]):
             x_residual = x_residual - proj_scale * Dz_pred_
 
         # Encode residual (novel part) with sparse SAE
-        z_novel = F.relu(torch.matmul(x_residual * self.lam, W_enc))
+        hidden_pre = self.hook_sae_acts_pre(torch.matmul(x_residual * self.lam, W_enc))
+        z_novel = F.relu(hidden_pre)
         if self.cfg.sae_diff_type == "topk":
             kval = self.cfg.kval_topk
             if kval is not None:
@@ -308,6 +309,7 @@ class TemporalSAE(SAE[TemporalSAEConfig]):
                 mask = torch.zeros_like(z_novel)
                 mask.scatter_(-1, topk_indices, 1)
                 z_novel = z_novel * mask
+        z_novel = self.hook_sae_acts_post(z_novel)
 
         # Return only novel codes (these are the interpretable features)
         return z_novel, z_pred
